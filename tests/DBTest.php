ed3f0da16938b8c67f9a75b22f86cc8d23c0ae60
Greg Lamb <email@greglamb.me>
2013-05-06 18:59:11 -0700

    updated tests

--- tests/DBTest.php
+++ tests/DBTest.php
@@ -4,54 +4,60 @@ use Lamb\PDOWrap\DB;
 
 class DBTest extends PHPUnit_Framework_TestCase {
 
-    public function testSelectVersion() {
+    protected $db;
+
+    protected function setUp() {
         $db = new DB('mysql:host=localhost;dbname=tests', 'root');
-        print_r(
-            $db->execute('SELECT version()')
-        );
     }
 
     public function testCreateTable() {
-        $db = new DB('mysql:host=localhost;dbname=tests', 'root');
-        print_r(
-            $db->execute('
+        $result = $this->db->execute('
                     CREATE TABLE test (
                      id INT,
                      data VARCHAR(100)
                     );
-            ')
-        );
+            ');
+        $this->assertTrue($result);
     }
 
     public function testInsert() {
-        $db = new DB('mysql:host=localhost;dbname=tests', 'root');
-        $db->execute('INSERT INTO test () VALUES (?, ?)',array(1, 'Hello'));
-        $db->execute('INSERT INTO test () VALUES (?, ?)',array(2, 'World'));
+        $result = $this->db->execute('INSERT INTO test () VALUES (?, ?)',array(1, 'Hello'));
+        $this->assertTrue($result);
+        $result = $this->db->execute('INSERT INTO test () VALUES (?, ?)',array(2, 'World'));
+        $this->assertTrue($result);
     }
 
     public function testSelectAll() {
-        $db = new DB('mysql:host=localhost;dbname=tests', 'root');
         $expected = array(
             array('id' => 1, 'data' => 'Hello')
             ,array('id' => 2, 'data' => 'World')
         );
-        $actual = $db->getAll('SELECT id, data FROM test');
+        $actual = $this->db->getAll('SELECT id, data FROM test');
         $this->assertEquals($expected, $actual);
     }
 
     public function testSelectOne() {
-        $db = new DB('mysql:host=localhost;dbname=tests', 'root');
         $expected = 2;
-        $actual = $db->getOne('SELECT id FROM test WHERE data = ?', array('World'));
+        $actual = $this->db->getOne('SELECT id FROM test WHERE data = ?', array('World'));
         $this->assertEquals($expected, $actual);
     }
 
     public function testSelectRow() {
-        $db = new DB('mysql:host=localhost;dbname=tests', 'root');
         $expected = array('id' => 1, 'data' => 'Hello');
-        $actual = $db->getRow('SELECT id, data FROM test WHERE data = ?', array('Hello'));
+        $actual = $this->db->getRow('SELECT id, data FROM test WHERE data = ?', array('Hello'));
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testFetch() {
+        $query = $this->db->query('SELECT id, data FROM test');
+
+        $expected = array('id' => 1, 'data' => 'Hello');
+        $result = $query->fetchRow();
         $this->assertEquals($expected, $actual);
 
+        $expected = array('id' => 2, 'data' => 'World');
+        $result = $query->fetchRow();
+        $this->assertEquals($expected, $actual);
     }
 
 }
